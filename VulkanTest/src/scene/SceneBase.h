72a9ae0bbb61cb46bf6a5626db0196241a3406a4
LeChenHz <405780108@qq.com>
2021-01-21 11:14:54 +0800

    17.帧缓冲framebuffer

--- VulkanTest/src/scene/SceneBase.h
+++ VulkanTest/src/scene/SceneBase.h
@@ -96,6 +96,7 @@ public:
 	VkFormat swapChainImageFormat;
 	VkExtent2D swapChainExtent;
 	std::vector<VkImageView> swapChainImageViews;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
 
 	VkRenderPass renderPass;
 	VkPipelineLayout pipelineLayout;
@@ -133,6 +134,7 @@ public:
 		createImageViews(); //创建图像视角
 		createRenderPass(); //创建渲染对象
 		createGraphicsPipeline(); //图形管线
+		createFramebuffers(); //帧缓冲
 	}
 
 
@@ -144,6 +146,9 @@ public:
 	}
  
 	virtual void cleanup(){
+		for (auto framebuffer : swapChainFramebuffers) {
+			vkDestroyFramebuffer(device, framebuffer, nullptr);
+		}
 		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 		vkDestroyRenderPass(device, renderPass, nullptr);
@@ -567,6 +572,30 @@ public:
 		vkDestroyShaderModule(device, fragShaderModule, nullptr);
 		vkDestroyShaderModule(device, vertShaderModule, nullptr);
 
+	}
+
+	virtual void createFramebuffers() {
+		swapChainFramebuffers.resize(swapChainImageViews.size());
+		for (size_t i = 0; i < swapChainImageViews.size(); i++)
+		{
+			VkImageView attachments[] = {
+				swapChainImageViews[i]
+			};
+
+			VkFramebufferCreateInfo framebufferInfo = {};
+			framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+			framebufferInfo.renderPass = renderPass;
+			framebufferInfo.attachmentCount = 1;
+			framebufferInfo.pAttachments = attachments;
+			framebufferInfo.width = swapChainExtent.width;
+			framebufferInfo.height = swapChainExtent.height;
+			framebufferInfo.layers = 1;
+
+			if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS)
+			{
+				throw std::runtime_error("failed to create framebuffer!");
+			}
+		}
 	}
 
 	virtual VkShaderModule createShaderModule(const std::vector<char>& code)
